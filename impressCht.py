4ce9088397fe2f5218d8eee592b7640d8372aa52
faridwijayaaa <faridwijayaaa@gmail.com>
2022-04-12 10:06:54 +0700

    Updated

---
+++ impressCht.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+print("Dimulai dlam 5dtk")
+time.sleep(5)
+
+for i in range(10):
+    pyautogui.write("Yakin nih?")
+    time.sleep(0.1)
+    pyautogui.press("Enter")
+    
